e441dc582fcd0f9489b383cc42adbe6a2ceaebe6
sw-develop <psw00719@naver.com>
2021-11-02 15:39:25 +0900

    [Assign1-2] Fix article/models.py
    Fix Article / Add Category, AComment, CComment

--- article/models.py
+++ article/models.py
@@ -1,19 +1,51 @@
 from djongo import models
 
 
-class Comment(models.Model):
+class CComment(models.Model):
     _id = models.ObjectIdField()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = "ccomment"
+
+
+class AComment(models.Model):
+    _id = models.ObjectIdField()
+    ccomments = models.ArrayField(
+        model_container=CComment
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "acomment"
+
+
+class Category(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "category"
+
 
 class Article(models.Model):
     _id = models.ObjectIdField()
-    comments = models.ArrayField(
-        model_container=Comment
+    acomments = models.ArrayField(
+        model_container=AComment
+    )
+    category = models.EmbeddedField(
+        model_container=Category
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "article"
